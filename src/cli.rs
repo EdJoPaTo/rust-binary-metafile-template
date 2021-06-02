c6df2dbbd7112e1756d9c78e4803f8062f7df40c
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-06-02 12:19:20 +0200

    feat(completions): add cli completions

---
+++ src/cli.rs
@@ -0,0 +1,10 @@
+use clap::{App, AppSettings};
+
+#[must_use]
+pub fn build() -> App<'static, 'static> {
+    App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .global_setting(AppSettings::ColoredHelp)
+}
