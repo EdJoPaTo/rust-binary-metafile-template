69e5a5b60d664d7f3ed9ef1773e8e73bb9de3c9a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-02-18 21:19:41 +0100

    feat(cli): show usage of env and value_hint
    This reminds me to create better clis

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,29 @@
-use clap::{command, Command};
+use clap::{command, Arg, Command, ValueHint};
 
+#[allow(clippy::too_many_lines)]
 #[must_use]
 pub fn build() -> Command<'static> {
     command!()
+        .arg(
+            Arg::new("name")
+                .long("name")
+                .env("USER")
+                .value_hint(ValueHint::Username)
+                .value_name("NAME")
+                .takes_value(true)
+                .default_value("world")
+                .help("Who to greet"),
+        )
+        .arg(
+            Arg::new("greeting")
+                .long("greeting")
+                .env("GREETING")
+                .value_hint(ValueHint::Other)
+                .value_name("GREETING")
+                .takes_value(true)
+                .default_value("Hello")
+                .help("Kind of greeting you want to get"),
+        )
 }
 
 #[test]
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod cli;
 
 fn main() {
     let matches = cli::build().get_matches();
-    println!("{:?}", matches);
-
-    println!("Hello, world!");
+    let greeting = matches.value_of("greeting").unwrap();
+    let name = matches.value_of("name").unwrap();
+    println!("{}, {}!", greeting, name);
 }
