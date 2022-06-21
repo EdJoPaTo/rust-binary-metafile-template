31be15dd82db00753b9b345bf3115b9b70deb1a6
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-06-21 18:20:07 +0200

    refactor: adapt to clap v3.2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod cli;
 
 fn main() {
     let matches = cli::build().get_matches();
-    let greeting = matches.value_of("greeting").unwrap();
-    let name = matches.value_of("name").unwrap();
+    let greeting = matches.get_one::<String>("greeting").unwrap();
+    let name = matches.get_one::<String>("name").unwrap();
     println!("{}, {}!", greeting, name);
 }
