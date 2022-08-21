b9ffa0e890fd984b2ae454342307536917accaca
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-08-21 10:29:39 +0200

    refactor(cli): migrate to clap_derive

--- src/cli.rs
+++ src/cli.rs
@@ -1,32 +1,29 @@
-use clap::{command, Arg, Command, ValueHint};
+use clap::{Parser, ValueHint};
 
-#[allow(clippy::too_many_lines)]
-#[must_use]
-pub fn build() -> Command<'static> {
-    command!()
-        .arg(
-            Arg::new("name")
-                .long("name")
-                .env("USER")
-                .value_hint(ValueHint::Username)
-                .value_name("NAME")
-                .takes_value(true)
-                .default_value("world")
-                .help("Who to greet"),
-        )
-        .arg(
-            Arg::new("greeting")
-                .long("greeting")
-                .env("GREETING")
-                .value_hint(ValueHint::Other)
-                .value_name("GREETING")
-                .takes_value(true)
-                .default_value("Hello")
-                .help("Kind of greeting you want to get"),
-        )
+#[derive(Debug, Parser)]
+#[clap(about, author, version)]
+pub struct Cli {
+    /// Who to greet
+    #[clap(
+        long,
+        env = "USER",
+        value_hint = ValueHint::Username,
+        default_value = "world",
+    )]
+    pub name: String,
+
+    /// Kind of greeting you want to get
+    #[clap(
+        long,
+        env = "GREETING",
+        value_hint = ValueHint::Other,
+        default_value = "Hello",
+    )]
+    pub greeting: String,
 }
 
 #[test]
 fn verify() {
-    build().debug_assert();
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use clap::Parser;
+
 mod cli;
 
 fn main() {
-    let matches = cli::build().get_matches();
-    let greeting = matches.get_one::<String>("greeting").unwrap();
-    let name = matches.get_one::<String>("name").unwrap();
-    println!("{}, {}!", greeting, name);
+    let matches = cli::Cli::parse();
+    println!("{}, {}!", matches.greeting, matches.name);
 }
