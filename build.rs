536b3d80fdc61fc866a7a60b7b7433cb30ea7cff
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-01-28 04:05:07 +0100

    build(completions): get shells dynamically from clap_complete

--- build.rs
+++ build.rs
@@ -1,23 +1,19 @@
-use clap::CommandFactory;
-use clap_complete::{generate_to, shells};
-
 include!("src/cli.rs");
 
 fn main() -> std::io::Result<()> {
+    use clap::{CommandFactory, ValueEnum};
+    const BIN_NAME: &str = env!("CARGO_PKG_NAME");
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cli.rs");
 
-    let out_dir = std::path::Path::new("target/completions/");
+    let out_dir = &std::path::Path::new("target").join("completions");
     drop(std::fs::remove_dir_all(out_dir));
     std::fs::create_dir_all(out_dir)?;
 
-    let bin_name = env!("CARGO_PKG_NAME");
-
-    generate_to(shells::Bash, &mut Cli::command(), bin_name, out_dir)?;
-    generate_to(shells::Elvish, &mut Cli::command(), bin_name, out_dir)?;
-    generate_to(shells::Fish, &mut Cli::command(), bin_name, out_dir)?;
-    generate_to(shells::PowerShell, &mut Cli::command(), bin_name, out_dir)?;
-    generate_to(shells::Zsh, &mut Cli::command(), bin_name, out_dir)?;
+    for &shell in clap_complete::Shell::value_variants() {
+        clap_complete::generate_to(shell, &mut Cli::command(), BIN_NAME, out_dir)?;
+    }
 
     Ok(())
 }
