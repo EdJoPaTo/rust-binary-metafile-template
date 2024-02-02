cd7bc2d8f07c6974ef617d2e3c1f98123b74a3c5
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-03 00:33:48 +0100

    build(man): generate manpages from clap

--- build.rs
+++ build.rs
@@ -7,13 +7,19 @@ fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cli.rs");
 
-    let out_dir = &std::path::Path::new("target").join("completions");
-    drop(std::fs::remove_dir_all(out_dir));
-    std::fs::create_dir_all(out_dir)?;
+    let target_dir = std::path::Path::new("target");
+    let compl_dir = &target_dir.join("completions");
+    let man_dir = &target_dir.join("manpages");
+    _ = std::fs::remove_dir_all(compl_dir);
+    _ = std::fs::remove_dir_all(man_dir);
+    std::fs::create_dir_all(compl_dir)?;
+    std::fs::create_dir_all(man_dir)?;
 
     for &shell in clap_complete::Shell::value_variants() {
-        clap_complete::generate_to(shell, &mut Cli::command(), BIN_NAME, out_dir)?;
+        clap_complete::generate_to(shell, &mut Cli::command(), BIN_NAME, compl_dir)?;
     }
+
+    clap_mangen::generate_to(Cli::command(), man_dir)?;
 
     Ok(())
 }
