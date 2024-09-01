a198b32909aaa93a448a3d6fef8c00a46db24b9d
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-09-01 21:39:04 +0200

    chore(copy): detect and reapply current rust-version

--- copy-template-into-existing-project.sh
+++ copy-template-into-existing-project.sh
@@ -12,6 +12,9 @@ git diff --quiet || (echo "repo unclean. stage or commit first" && exit 1)
 name=$(basename "$PWD")
 templatedir="$(dirname "$0")"
 
+# Set the default of 1.63, the version on Debian bullseye
+rustversion=$(rg --replace='$1' '^rust-version\s*=\s*"(.+)"$' Cargo.toml || echo "1.63")
+
 rm -rf .github/workflows/
 cp -r \
 	"$templatedir/"{Cargo.toml,rustfmt.toml,.github,.gitignore,.dockerignore,Dockerfile,build.rs,systemd} \
@@ -22,5 +25,8 @@ echo "everything copied"
 # Replace template name with folder name
 # macOS: add '' after -i like this: sed -i '' "s/…
 sed -i "s/rust-binary-metafile-template/$name/g" Cargo.toml Dockerfile .github/**/*.yml systemd/**/*
+
+sed -i "s/rust-version = .*/rust-version = \"$rustversion\"/g" Cargo.toml
+sed -i "s/- '1.74'/- '$rustversion'/g" .github/**/*.yml
 
 git --no-pager status --short
